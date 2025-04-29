e5273fdceb65e0a0ca6c3b075ac4e19fd0dd804d
darrield <darrieldapples@gmail.com>
2025-04-29 20:34:52 +0700

    membuat view berita

--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', function() {
+    return view('beranda', ['name' => 'Darriel Dapples',
+    'email' => 'darrieldapples@gmail.com', 'address' => 'Jalan Jendral Sudirman']);
+});
+
+Route::get('/berita/{id}/{judul?}', function($id,$judul= null) {
+    return view ('berita', ['id' => $id, 'judul' => $judul ]);
+});
